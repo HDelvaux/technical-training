2e906b82040b85cd536c7e67b480fa0cd310cc31
Herve Delvaux <hde@odoo.com>
2018-09-17 16:03:22 +0200

    first attempt

---
+++ library/models/library.py
@@ -0,0 +1,35 @@
+from odoo import api, models, fields
+
+class Book(models.Model)
+
+    _name = 'library.book'
+    
+    authors = fields.Many2many('library.author')
+    editors = fields.Many2many('library.editors')
+    customers = fields.Many2many('library.customer')
+    edition_year = fields.Integer()
+    ISBN = fields.Integer()
+
+
+class Author(models.Model)
+
+    _name = 'library.author'
+
+    name = fields.Char()
+    books = fields.Many2many('library.book')
+    
+class Editor(models.Model)
+
+    _name = 'library.editor'
+
+    name = fields.Char()
+    books = fields.Many2many('library.book')
+    
+class Customer(models.Model)
+
+    _name = 'library.customer'
+
+    name = fields.Char()
+    address = fields.Char()
+    emails = fields.Char()
+    books  = fileds.Many2many('library.book')
