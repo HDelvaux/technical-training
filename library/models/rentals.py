b5f3125c981898a2aa5bec60430e09606216ed91
Herve Delvaux <hde@odoo.com>
2018-09-19 15:25:22 +0000

    done

--- library/models/rentals.py
+++ library/models/rentals.py
@@ -9,3 +9,16 @@ class Rentals(models.Model):
     book_id = fields.Many2one('library.book', string='Book')
     rental_date = fields.Date(string='Rental date')
     return_date = fields.Date(string='Return date')
+    
+    book_name = fields.Char(related='book_id.name')
+    book_authors_ids = fields.Many2many(related='book_id.authors_ids')
+    book_edition_date =  fields.Date(related='book_id.edition_date')
+    book_isbn = fields.Char(related='book_id.isbn')
+    book_publisher_id = fields.Many2one(related='book_id.publisher_id')
+    #book_rental_ids = fields.Char(related='library.book.rental_ids.')
+    
+    customer_name = fields.Char(related='customer_id.name')
+    customer_email = fields.Char(related='customer_id.email')
+    customer_address = fields.Text(related='customer_id.address')
+    customer_partner_type = fields.Selection(related='customer_id.partner_type')
+    #customer_rental_ids = fields.Char(related='library.partner.rental_ids.')
--- openacademy/models.py
+++ openacademy/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Courses(models.Model):
     _name = 'openacademy.course'
@@ -17,3 +18,15 @@ class Sessions(models.Model):
     start_date = fields.Date()
     seats = fields.Integer('Room Capacity')
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    attendee_count = fields.Integer(compute='_compute_total_attendee')
+    
+    @api.depends('attendee_ids')
+    def _compute_total_attendee(self):
+        for record in self:
+            record.attendee_count = len(record.attendee_ids)
+
+    @api.constrains('attendee_ids', 'seats')
+    def _check_total_attendee(self):
+        for record in self:
+            if record.attendee_count  > record.seats:
+                raise ValidationError("Too many attendees")
